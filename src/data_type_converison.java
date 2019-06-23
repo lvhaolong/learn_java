552c11876953e8c478b18a0ab28ee1dc610369d9
lvhaolong <769653743@qq.com>
2019-06-23 10:04:04 +0800

    init

---
+++ src/data_type_converison.java
@@ -0,0 +1,7 @@
+public class data_type_converison {
+    public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+        System.out.println(a+b);
+    }
+}
---
+++ src/date.java
@@ -0,0 +1,9 @@
+class date {
+    public static void main(String[] args) {
+        int b1=3,b2=4;
+        System.out.println(b1+b2);
+
+
+    }
+    
+}
